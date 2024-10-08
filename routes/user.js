f0f24fa3bbd3ce1da8da9929a2e9112900ba9a81
SifEddine05 <sifousamir72@gmail.com>
2024-10-08 23:33:15 +0100

    add get me route

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { register, login, logout } = require('../controllers/user');
+const { register, login, logout, getMe } = require('../controllers/user');
+const authenticateToken = require('../middlewares/auth.middleware');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/me',authenticateToken,getMe);
 router.post('/logout', logout);
 
 module.exports = router;
